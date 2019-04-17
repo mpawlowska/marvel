131fa919bafda40e7314df4d084f14e1d483019f
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2019-04-17 10:41:54 +0200

    Setup with create-react-app, change eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['airbnb'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-one-expression-per-line': 0
+  },
+  env: {
+    browser: true,
+    jest: true
+  }
+};
