d23066a2bc47543ff3f151379f176fcdbac6c89a
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2019-04-24 23:34:12 +0200

    Change content
    Add basic layout wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,9 @@ import styles from './App.module.scss';
 
 export const App = () => (
   <div className={styles.app}>
-    <header className={styles.appHeader}>
-      <p>
-        Edit <div>src/App.js</div> and save to reload.
-      </p>
-      <div>Learn React</div>
-    </header>
-    <p className={styles.new}>Something new here</p>
+    <div className={styles.contentWrapper}>
+      <header className={styles.appHeader}>Space-X</header>
+      <main>Some content will be here</main>
+    </div>
   </div>
 );
