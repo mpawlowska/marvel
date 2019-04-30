40a1b24739869d7d201846d43d262adcc494ec21
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2019-04-30 17:43:50 +0200

    Add image to character item

--- src/components/CharacterItem/CharacterItem.js
+++ src/components/CharacterItem/CharacterItem.js
@@ -2,18 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CharacterItem.module.scss';
 
-const CharacterItem = ({ data: { name } }) => (
-  <div className={styles.characterItem}>{name}</div>
-);
+const CharacterItem = ({
+  data: {
+    thumbnail: { path, extension },
+    name
+  }
+}) => {
+  const characterImg = `${path}.${extension}`;
+
+  return (
+    <div className={styles.characterItem}>
+      <img
+        alt="Character"
+        src={characterImg}
+        className={styles.characterImage}
+      />
+      <p>{name}</p>
+    </div>
+  );
+};
 
 CharacterItem.propTypes = {
   data: PropTypes.shape({
+    thumbnail: PropTypes.shape({
+      path: PropTypes.string,
+      extension: PropTypes.string
+    }),
     name: PropTypes.string.isRequired
   })
 };
 
 CharacterItem.defaultProps = {
   data: {
+    thumbnail: {
+      path: '',
+      extension: ''
+    },
     name: ''
   }
 };
