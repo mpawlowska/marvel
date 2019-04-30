43c51099c976af83c8df06c2504333d1e1849ec0
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2019-04-30 14:46:28 +0200

    Handle error on fetching characters

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './CharacterList.module.scss';
 import { API_KEY, CHARACTERS_URL } from '../../constants/api';
@@ -7,32 +7,46 @@ import CharacterItem from '../CharacterItem/CharacterItem';
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
+      setIsError(false);
 
-      const {
-        data: { data = { results: [] } }
-      } = await axios.get(CHARACTERS_URL, {
-        params: {
-          apikey: API_KEY
-        }
-      });
-      setCharacters(data.results);
+      try {
+        const {
+          data: { data = { results: [] } }
+        } = await axios.get(CHARACTERS_URL, {
+          params: {
+            apikey: API_KEY
+          }
+        });
+        setCharacters(data.results);
+      } catch (err) {
+        setIsError(true);
+      }
       setIsLoading(false);
     };
+
     fetchData();
   }, []);
 
-  return isLoading ? (
-    <div>Heros are arriving...</div>
-  ) : (
-    <ul className={styles.characterList}>
-      {characters.map((character, i) => (
-        <CharacterItem key={i} data={character} />
-      ))}
-    </ul>
+  return (
+    <Fragment>
+      {isError && <div>Something went wrong... Heros didn't arrive</div>}
+      {isLoading && <div>Heros are arriving...</div>}
+      {!isLoading &&
+        (characters.length === 0 ? (
+          <div>No hero found</div>
+        ) : (
+          <ul className={styles.characterList}>
+            {characters.map((character, i) => (
+              <CharacterItem key={i} data={character} />
+            ))}
+          </ul>
+        ))}
+    </Fragment>
   );
 };
 
